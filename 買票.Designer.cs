eef6f25df84928619d83db5309250bba9208fca1
xingmingg <71859234+xingmingg@users.noreply.github.com>
2021-05-13 15:32:11 +0700

    Add files via upload

---
+++ 買票.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class 買票
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLocal = new System.Windows.Forms.Button();
+            this.btnForeign = new System.Windows.Forms.Button();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ticket";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Number of Ticket";
+            // 
+            // btnLocal
+            // 
+            this.btnLocal.Location = new System.Drawing.Point(221, 268);
+            this.btnLocal.Name = "btnLocal";
+            this.btnLocal.Size = new System.Drawing.Size(75, 23);
+            this.btnLocal.TabIndex = 1;
+            this.btnLocal.Text = "Local People";
+            this.btnLocal.UseVisualStyleBackColor = true;
+            this.btnLocal.Click += new System.EventHandler(this.btnLocal_Click);
+            // 
+            // btnForeign
+            // 
+            this.btnForeign.Location = new System.Drawing.Point(363, 268);
+            this.btnForeign.Name = "btnForeign";
+            this.btnForeign.Size = new System.Drawing.Size(75, 23);
+            this.btnForeign.TabIndex = 1;
+            this.btnForeign.Text = "Foreigner";
+            this.btnForeign.UseVisualStyleBackColor = true;
+            this.btnForeign.Click += new System.EventHandler(this.btnForeign_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(331, 93);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 23);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(331, 146);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 23);
+            this.txtNum.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(331, 197);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 23);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // 買票
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.btnForeign);
+            this.Controls.Add(this.btnLocal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "買票";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLocal;
+        private System.Windows.Forms.Button btnForeign;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ 買票.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class 買票 : Form
+    {
+        public 買票()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTotal.Enabled = false;
+
+        }
+
+        private void btnLocal_Click(object sender, EventArgs e)
+        {
+            int userId;
+            if (int.TryParse(txtNum.Text, out userId) && int.TryParse(txtHeight.Text, out userId))
+            {
+               
+                float height; 
+                string total;
+                 
+                height = int.Parse(txtHeight.Text);
+                if(height>1.40)
+                {
+                    //MessageBox.Show("Tryagain");
+                   double num1 = Convert.ToInt32(txtNum.Text)*5;
+                    txtTotal.Text = num1.ToString("$0.00");
+                }
+               // else if(height>1.40)
+               // {
+                    
+               // }
+                else
+                {
+                    double num2 = Convert.ToInt32(txtNum.Text)*5/2;
+                    txtTotal.Text = num2.ToString("$0.00");
+
+                    // MessageBox.Show("Try elseagain");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please input something!!!");
+            }
+        }
+
+        private void btnForeign_Click(object sender, EventArgs e)
+        {
+            int userId;
+            if (int.TryParse(txtNum.Text, out userId) && int.TryParse(txtHeight.Text, out userId))
+            {
+
+                double height;
+             //   string total;
+
+                height = int.Parse(txtHeight.Text);
+                if (height > 1.40)
+                {
+                    //MessageBox.Show("Tryagain");
+                    double num1 = Convert.ToInt32(txtNum.Text) * 10;
+                    txtTotal.Text = num1.ToString("$0.00");
+                }
+                // else if(height>1.40)
+                // {
+
+                // }
+                else
+                {
+                    double num2 = Convert.ToInt32(txtNum.Text) * 10 / 2;
+                    txtTotal.Text = num2.ToString("$00.00");
+
+                    // MessageBox.Show("Try elseagain");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please input something!!!");
+            }
+        }
+    }
+}
